7065cf9860fc905b402a1742bf3fd0dcb546e38e
David Robert-Ansart <david.robertansart@gmail.com>
2024-02-22 11:37:31 +0100

    Add image route

---
+++ src/app/api/images/[name]/route.ts
@@ -0,0 +1,116 @@
+import mime from 'mime-types';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import { Readable } from 'stream';
+
+import { getFile, getFileData } from 'src/fileUpload';
+
+export const dynamic = 'force-dynamic';
+
+const notFoundResponse = () => {
+    return new NextResponse('Error 404, not found.', {
+        status: 404,
+    });
+};
+const getContentTypeFromFileName = (filename: string): string | null => mime.lookup(filename) || null;
+
+export async function GET(request: NextRequest, { params }: { params: { name: string } }) {
+    const data = await getFileData('images', params.name);
+
+    if (!data) {
+        return notFoundResponse();
+    }
+
+    const size = data.ContentLength;
+    const range = request.headers.get('range');
+    const contentType =
+        data.ContentType.length === 0 || data.ContentType === 'application/octet-stream'
+            ? getContentTypeFromFileName(params.name) ?? 'application/octet-stream'
+            : data.ContentType ?? 'application/octet-stream';
+
+    // file does not exists
+    if (size === 0) {
+        return notFoundResponse();
+    }
+
+    const readable = (await getFile('images', params.name, range || undefined))?.on('error', () => {
+        return notFoundResponse();
+    });
+    if (!readable) {
+        return notFoundResponse();
+    }
+    const headers = new Headers({
+        'Last-Modified': data.LastModified.toISOString(),
+        'Content-Type': contentType,
+    });
+
+    /** Check for Range header */
+    if (range) {
+        /** Extracting Start and End value from Range Header */
+        const [startStr, endStr] = range.replace(/bytes=/, '').split('-');
+        let start = parseInt(startStr, 10);
+        let end = endStr ? parseInt(endStr, 10) : size - 1;
+
+        if (!isNaN(start) && isNaN(end)) {
+            end = size - 1;
+        }
+        if (isNaN(start) && !isNaN(end)) {
+            start = size - end;
+            end = size - 1;
+        }
+
+        // Handle unavailable range request
+        if (start >= size || end >= size) {
+            // Return the 416 Range Not Satisfiable.
+            const response = new Response(null, {
+                status: 416,
+                headers,
+            });
+            response.headers.set('Content-Range', `bytes */${size}`);
+            return response;
+        }
+
+        /** Sending Partial Content With HTTP Code 206 */
+        const response = new Response(Readable.toWeb(readable) as ReadableStream, {
+            status: 206,
+            headers,
+        });
+        response.headers.set('Accept-Ranges', 'bytes');
+        response.headers.set('Content-Range', `bytes ${start}-${end}/${size}`);
+        response.headers.set('Content-Length', `${end - start + 1}`);
+        return response;
+    } else {
+        const response = new Response(Readable.toWeb(readable) as ReadableStream, {
+            status: 200,
+            headers,
+        });
+        response.headers.set('Content-Length', `${data.ContentLength}`);
+        return response;
+    }
+}
+
+export async function HEAD(_request: NextRequest, { params }: { params: { name: string } }) {
+    const data = await getFileData('images', params.name);
+    if (!data) {
+        return notFoundResponse();
+    }
+
+    const size = data.ContentLength;
+    const contentType =
+        data.ContentType.length === 0 || data.ContentType === 'application/octet-stream'
+            ? getContentTypeFromFileName(params.name) ?? 'application/octet-stream'
+            : data.ContentType ?? 'application/octet-stream';
+    // file does not exists
+    if (size === 0) {
+        return notFoundResponse();
+    }
+
+    const response = new NextResponse(null, {
+        status: 200,
+    });
+    response.headers.set('Accept-Ranges', 'bytes');
+    response.headers.set('Content-Length', `${data.ContentLength}`);
+    response.headers.set('Last-Modified', data.LastModified.toISOString());
+    response.headers.set('Content-Type', contentType);
+    return response;
+}
