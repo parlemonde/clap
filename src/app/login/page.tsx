f1c7d7207f7dd5607e221468a0c8bff1c8173cc8
David Robert-Ansart <david.robertansart@gmail.com>
2024-06-30 14:35:15 +0200

    Update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { EyeNoneIcon, EyeOpenIcon } from '@radix-ui/react-icons';
 import { redirect } from 'next/navigation';
 import * as React from 'react';
 import { useFormState } from 'react-dom';
 
 import { login } from 'src/actions/authentication/login';
+import { Button } from 'src/components/layout/Button';
+import { IconButton } from 'src/components/layout/Button/IconButton';
+import { Container } from 'src/components/layout/Container';
+import { Field, Form, Input } from 'src/components/layout/Form';
+import { Title } from 'src/components/layout/Typography';
 import { useTranslation } from 'src/contexts/translationContext';
 import { userContext } from 'src/contexts/userContext';
 
@@ -12,27 +18,53 @@ export default function LoginPage() {
     const [message, formAction] = useFormState(login, '');
     const { t } = useTranslation();
 
+    const [showPassword, setShowPassword] = React.useState(false);
+
     const { user } = React.useContext(userContext);
     if (user) {
         redirect('/');
     }
 
     return (
-        <div>
-            <p>LOGIN</p>
-            <form action={formAction}>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email" autoComplete="email" />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
-                </div>
-
-                <button type="submit">{t('login')}</button>
-                {message}
-            </form>
-        </div>
+        <Container className="text-center">
+            <Title color="primary" variant="h1" marginTop={48} marginBottom="lg">
+                {t('login_title')}
+            </Title>
+            <Form className="login-form" action={formAction}>
+                {message && <span style={{ color: 'rgb(211, 47, 47)', display: 'block' }}>{message}</span>}
+                <Field
+                    name="email"
+                    label={<span style={{ display: 'inline-block', marginBottom: 4 }}>{t('login_username')}</span>}
+                    input={<Input id="email" name="email" type="text" color="secondary" isFullWidth required />}
+                ></Field>
+                <Field
+                    name="password"
+                    label={<span style={{ display: 'inline-block', marginBottom: 4 }}>{t('login_password')}</span>}
+                    input={
+                        <Input
+                            type={showPassword ? 'text' : 'password'}
+                            id="password"
+                            name="password"
+                            color="secondary"
+                            isFullWidth
+                            required
+                            iconAdornment={
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={() => {
+                                        setShowPassword(!showPassword);
+                                    }}
+                                    variant="borderless"
+                                    icon={showPassword ? EyeNoneIcon : EyeOpenIcon}
+                                    iconProps={{ style: { color: 'rgba(0, 0, 0, 0.54)', height: 24, width: 24 } }}
+                                ></IconButton>
+                            }
+                            iconAdornmentProps={{ position: 'right' }}
+                        />
+                    }
+                ></Field>
+                <Button label={t('login_connect')} variant="contained" color="secondary" type="submit" value="Submit"></Button>
+            </Form>
+        </Container>
     );
 }
