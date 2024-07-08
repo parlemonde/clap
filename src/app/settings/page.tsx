5cfbf5277ae1557d2b71fb53c612f310e011088c
David Robert-Ansart <david.robertansart@gmail.com>
2024-07-08 20:55:32 +0200

    Add settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,45 @@
 import * as React from 'react';
 
-export default function SettingsPage() {
+import { getTranslation } from 'src/actions/get-translation';
+import { Container } from 'src/components/layout/Container';
+import { Field, Form } from 'src/components/layout/Form';
+import { Select } from 'src/components/layout/Form/Select';
+import { Title } from 'src/components/layout/Typography';
+
+export default async function SettingsPage() {
+    const { t, currentLocale } = await getTranslation();
+    const languages = [
+        {
+            label: 'Français',
+            value: 'fr',
+        },
+        {
+            label: 'English',
+            value: 'en',
+        },
+    ];
+
     return (
-        <main>
-            <h1>Settings</h1>
-        </main>
+        <Container paddingBottom="xl">
+            <Title marginY="md">{t('settings')}</Title>
+            <Title color="inherit" variant="h2" marginTop="sm" marginBottom="md" style={{ width: '100%', textAlign: 'left' }}>
+                {t('change_language')}
+            </Title>
+            <Form preventSubmit>
+                <Field
+                    name="language"
+                    label={t('language')}
+                    input={
+                        <Select name="language" id="language" marginTop="xs" isFullWidth value={currentLocale}>
+                            {languages.map((l) => (
+                                <option value={l.value} key={l.value}>
+                                    {l.label}
+                                </option>
+                            ))}
+                        </Select>
+                    }
+                ></Field>
+            </Form>
+        </Container>
     );
 }
