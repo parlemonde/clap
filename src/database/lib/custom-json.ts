bc8f40d08c2e30fb430a96d9891f4278405c393f
David Robert-Ansart <david.robertansart@gmail.com>
2024-08-18 11:10:22 +0200

    Update dependancies

--- src/database/lib/custom-json.ts
+++ src/database/lib/custom-json.ts
@@ -8,6 +8,7 @@ export type CustomMySqlJsonBuilderInitial<TName extends string, DataType = unkno
     data: DataType;
     driverParam: unknown;
     enumValues: undefined;
+    generated: undefined;
 }>;
 
 export const json = postgresqlJson as <TName extends string, DataType = unknown>(name: TName) => CustomMySqlJsonBuilderInitial<TName, DataType>;
