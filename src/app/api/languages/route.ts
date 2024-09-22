b9be3f696939981e67abc64bd51c9750da827097
David Robert-Ansart <david.robertansart@gmail.com>
2024-09-22 17:32:49 +0200

    Fix languages route

--- src/app/api/languages/route.ts
+++ src/app/api/languages/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 
 import { db } from 'src/database';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     const languages = await db.query.languages.findMany();
     return NextResponse.json(languages);
