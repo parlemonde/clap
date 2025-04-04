a7d4d0f92a5ac26e6fa781f47feace4e1f6fef72
David Robert-Ansart <david.robertansart@gmail.com>
2025-04-04 18:21:37 +0200

    Rename to server preview proxy

--- server-preview-proxy/src/event_handler.rs
+++ server-preview-proxy/src/event_handler.rs
@@ -4,6 +4,7 @@ use aws_lambda_events::event::apigw::ApiGatewayV2httpResponse;
 use aws_lambda_events::http::HeaderMap;
 use aws_sdk_lambda::primitives::Blob;
 use lambda_runtime::{Error, LambdaEvent};
+use std::env;
 
 fn get_unknown_error() -> ApiGatewayV2httpResponse {
     ApiGatewayV2httpResponse {
@@ -18,13 +19,15 @@ fn get_unknown_error() -> ApiGatewayV2httpResponse {
 
 async fn get_response(
     payload: &ApiGatewayV2httpRequest,
+    hash: &str,
 ) -> Result<ApiGatewayV2httpResponse, Error> {
     let payload = serde_json::to_string(payload)?;
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_lambda::Client::new(&config);
+    let function_name_prefix = env::var("FUNCTION_NAME_PREFIX")?;
     let invoke_result = client
         .invoke()
-        .function_name("clap-prod")
+        .function_name(format!("{}-{}", function_name_prefix, hash))
         .payload(Blob::new(payload))
         .send()
         .await;
@@ -40,7 +43,16 @@ async fn get_response(
 pub(crate) async fn function_handler(
     event: LambdaEvent<ApiGatewayV2httpRequest>,
 ) -> Result<ApiGatewayV2httpResponse, Error> {
-    let response = get_response(&event.payload).await;
+    let hash = event.payload.headers.get("x-preview-hash");
+    let hash = match hash {
+        Some(header_value) => header_value.to_str().unwrap_or(""),
+        None => ""
+    };
+    if hash.len() == 0 {
+        return Ok(get_unknown_error());
+    }
+
+    let response = get_response(&event.payload, hash).await;
     if response.is_err() {
         return Ok(get_unknown_error());
     }
