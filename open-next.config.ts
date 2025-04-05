3064b8095a7e72898ca5325d69f7f6460f04fd2f
David Robert-Ansart <david.robertansart@gmail.com>
2025-04-05 17:15:03 +0200

    Test using x64

--- open-next.config.ts
+++ open-next.config.ts
@@ -4,7 +4,7 @@ const config: OpenNextConfig = {
     default: {
         install: {
             packages: ['sharp@0.33.5', '@node-rs/argon2@2.0.2'],
-            additionalArgs: '--platform=linux --os=linux --arch=arm64 --cpu=arm64 --libc=glibc',
+            // additionalArgs: '--platform=linux --os=linux --arch=arm64 --cpu=arm64 --libc=glibc',
         },
         override: {
             tagCache: 'dummy',
