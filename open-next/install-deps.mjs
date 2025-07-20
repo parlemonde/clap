32c4abb8c00dc63e431dfd9bcbe049e7095cff7e
David Robert-Ansart <david.robertansart@gmail.com>
2025-07-20 11:54:46 +0200

    Update open-next

--- open-next/install-deps.mjs
+++ open-next/install-deps.mjs
@@ -5,6 +5,5 @@ import fs from 'node:fs';
 fs.rmSync('.open-next/server-functions/default/node_modules/sharp', { recursive: true, force: true });
 
 installDependencies('.open-next/server-functions/default', {
-    packages: ['sharp@0.34.2', '@node-rs/argon2@2.0.2'],
-    // additionalArgs: '--platform=linux --os=linux --arch=arm64 --cpu=arm64 --libc=glibc',
+    packages: ['sharp@0.34.3', '@node-rs/argon2@2.0.2'],
 });
