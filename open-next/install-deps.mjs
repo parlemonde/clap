db4fa0512f2169ee7b396cc4918c470612ea176b
David Robert-Ansart <david.robertansart@gmail.com>
2026-03-29 21:42:28 +0200

    Remove open next

--- open-next/install-deps.mjs
+++
@@ -1,9 +0,0 @@
-import { installDependencies } from '@opennextjs/aws/build/installDeps.js';
-import fs from 'node:fs';
-
-// Remove broken sharp symlink first.
-fs.rmSync('.open-next/server-functions/default/node_modules/sharp', { recursive: true, force: true });
-
-installDependencies('.open-next/server-functions/default', {
-    packages: ['sharp@0.34.3', '@node-rs/argon2@2.0.2'],
-});
