a35197efdad264f6da951102d11c3b2c498c5d70
David Robert-Ansart <david.robertansart@gmail.com>
2026-04-18 20:42:18 +0200

    Fix i18n import issue

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 import path from 'path';
 
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+const withNextIntl = createNextIntlPlugin('./src/server/i18n/request.ts');
 
 const nextConfig: NextConfig = {
     poweredByHeader: false,
