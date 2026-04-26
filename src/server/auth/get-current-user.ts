c7ff4fcd549a864485122aa63f7296f6ef5b8c68
David Robert-Ansart <david.robertansart@gmail.com>
2026-04-26 13:44:06 +0200

    Add get auth span

--- src/server/auth/get-current-user.ts
+++ src/server/auth/get-current-user.ts
@@ -1,3 +1,4 @@
+import { trace } from '@opentelemetry/api';
 import { headers } from 'next/headers';
 
 import type { User, UserRole } from '@server/database/schemas/users';
@@ -9,30 +10,46 @@ const isValidUserRole = (role: string | null | undefined): role is UserRole => {
     return role === 'admin' || role === 'teacher' || role === 'student';
 };
 
+const tracer = trace.getTracer('auth');
+
 export const getCurrentUser = async (): Promise<User | undefined> => {
-    const session = await auth.api.getSession({
-        headers: await headers(),
+    return tracer.startActiveSpan('getCurrentUser', async (span): Promise<User | undefined> => {
+        const session = await auth.api.getSession({
+            headers: await headers(),
+        });
+        if (!session) {
+            return undefined;
+        }
+
+        const userRole = isValidUserRole(session.user.role) ? session.user.role : 'teacher';
+        const sessionData = await getActiveStudentCollaborationSessionData(session.session.data);
+
+        if (sessionData) {
+            span.setAttributes({
+                'user.id': 'temporary-student',
+                'user.email': '',
+                'user.name': 'student',
+            });
+
+            return {
+                ...session.user,
+                role: 'student',
+                projectId: sessionData.projectId,
+                questionId: sessionData.questionId,
+                teacherId: sessionData.teacherId,
+            };
+        }
+
+        if (session.user.email === STUDENT_COLLABORATION_USER_EMAIL || userRole === 'student') {
+            return undefined;
+        }
+
+        span.setAttributes({
+            'user.id': session.user.id,
+            'user.email': session.user.email,
+            'user.name': session.user.name,
+        });
+
+        return { ...session.user, role: userRole };
     });
-    if (!session) {
-        return undefined;
-    }
-
-    const userRole = isValidUserRole(session.user.role) ? session.user.role : 'teacher';
-    const sessionData = await getActiveStudentCollaborationSessionData(session.session.data);
-
-    if (sessionData) {
-        return {
-            ...session.user,
-            role: 'student',
-            projectId: sessionData.projectId,
-            questionId: sessionData.questionId,
-            teacherId: sessionData.teacherId,
-        };
-    }
-
-    if (session.user.email === STUDENT_COLLABORATION_USER_EMAIL || userRole === 'student') {
-        return undefined;
-    }
-
-    return { ...session.user, role: userRole };
 };
